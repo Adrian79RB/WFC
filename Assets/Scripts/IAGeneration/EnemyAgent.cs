00f674258c7b485ad3239c4a83f0c23b422b659b
AdrianRamosBoira <78815548+AdrianRamosBoira@users.noreply.github.com>
2022-02-03 20:56:49 +0100

    Patrol method

--- Assets/Scripts/IAGeneration/EnemyAgent.cs
+++ Assets/Scripts/IAGeneration/EnemyAgent.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAgent : MonoBehaviour
 {
+    [Header("Making Decisions")]
     // Constants used to decide in the tree
     public float maxHealth;
     public float maxAmmo;
@@ -14,7 +16,13 @@ public class EnemyAgent : MonoBehaviour
     public float hitDistance;
 
     // Behaviour block Set
+    [Header("Block Set")]
     public Block[] enemyBlockSet;
+
+    // Enemy Functional stuff
+    [Header("Enemy Functionality stuff")]
+    public NavMeshAgent agent;
+    public Transform[] waypoints;
 
     // Game data container to use the BehviourBlocks
     Dictionary<string, float> gameData; // keys: playerDetected, health, maxHealth, allyNum, ammo, maxAmmo, distanceToPlayer, safeDistance, AttackDistance, hitDistance, shootDistance
@@ -24,6 +32,17 @@ public class EnemyAgent : MonoBehaviour
     float ammo;
     float health;
     EnemyAgent[] allies;
+    int allyNum;
+    GameObject player;
+
+    // Enemy Functional variables
+    int waypointIndex;
+    float waitTimer = 5.0f;
+    Transform currentWaypoint;
+
+    // Variables that check the gameData Update
+    float gameDataUpdateTimer = 5.0f;
+    float gameDataUpdateTime = 0.0f;
 
     // BehaviourGenerator and BehaviourTree
     BehaviourBlockGeneration treeGenerator;
@@ -79,13 +98,83 @@ public class EnemyAgent : MonoBehaviour
 
     void Update()
     {
-
+        gameDataUpdateTime += Time.deltaTime;
+        if(gameDataUpdateTime >= gameDataUpdateTimer)
+        {
+            gameDataUpdateTime = 0.0f;
+            GetGameData();
+        }
+        
         //currentBlock = currentBlock.Run(this, gameData);
+    }
+
+    private void GetGameData()
+    {
+        if(gameData.Count > 0)
+        {
+            if (playerDetected)
+                gameData["playerDetected"] = 1.0f;
+            else
+                gameData["playerDetected"] = 0.0f;
+
+            gameData["health"] = health;
+            gameData["ammo"] = ammo;
+            gameData["allyNum"] = allyNum;
+
+            var distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            gameData["distanceToPlayer"] = distanceToPlayer;
+        }
+        else
+        {
+            if (playerDetected)
+                gameData.Add("playerDetected", 1.0f);
+            else
+                gameData.Add("playerDetected", 0.0f);
+
+            gameData.Add("health", health);
+            gameData.Add("maxHealth", maxHealth);
+            gameData.Add("ammo", ammo);
+            gameData.Add("maxAmmo", maxAmmo);
+            gameData.Add("allyNum", allyNum);
+            gameData.Add("safeDistance", safeDistance);
+            gameData.Add("AttackDistance", AttackDistance);
+            gameData.Add("hitDistance", hitDistance);
+            gameData.Add("ShootDistance", ShootDistance);
+
+            var distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            gameData.Add("distanceToPlayer", distanceToPlayer);
+        }
     }
 
     public void GoPatrolling()
     {
-        throw new NotImplementedException();
+        if (!playerDetected)
+        {
+            if(Vector3.Distance(currentWaypoint.position, transform.position) < agent.stoppingDistance)
+            {
+                if (waypointIndex >= waypoints.Length)
+                    waypointIndex = 0;
+
+                if (UnityEngine.Random.value > 0.2f)
+                    currentWaypoint = waypoints[waypointIndex];
+                else
+                    agent.isStopped = true;
+            }
+
+            if (agent.isStopped)
+            {
+                waitTimer -= Time.deltaTime;
+                if(waitTimer <= 0f)
+                {
+                    waitTimer = 5.0f;
+                    agent.isStopped = false;
+                }
+            }
+            else 
+            {
+                agent.Move(currentWaypoint.position);
+            }
+        }
     }
 
     internal void RetreatToHome()
