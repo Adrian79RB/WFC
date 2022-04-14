1d9e9566aaa2d083ab30d40b7fa7aede60b851ad
AdrianRamosBoira <78815548+AdrianRamosBoira@users.noreply.github.com>
2022-04-14 14:04:27 +0200

    CrowdAnimated

---
+++ Assets/Scripts/Crowd/CrowdGeneration.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdGeneration : MonoBehaviour
+{
+    public Transform colliseumCentre;
+    public GameObject[] crowdCharacters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var pos = Mathf.RoundToInt(Random.value * (crowdCharacters.Length - 1));
+            Vector3 dir = (colliseumCentre.position - transform.GetChild(i).position).normalized;
+            Quaternion newRotation = Quaternion.FromToRotation(transform.GetChild(i).forward, dir);
+            Transform newCharacter = Instantiate(crowdCharacters[pos], transform.GetChild(i).position, newRotation, transform.GetChild(i)).transform;
+
+            newCharacter.rotation = Quaternion.Euler(0f, newCharacter.rotation.eulerAngles.y, 0f);
+        }
+    }
+}
